565a748d4935ab81ca54e5a57a6b57355268569b
calvin <cv2k10@gmail.com>
2021-06-16 16:40:23 +0800

    react-router-dom with sample codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
